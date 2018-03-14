f1a59ca0b1fb65d5e48c2daa8c5ac6890f7aebaf
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-03-14 19:56:07 +1100

    tested to Hacker Rank problem

--- Lucas-theorem.rkt
+++ Lucas-theorem.rkt
@@ -1,5 +1,13 @@
 #lang racket
-(require math/number-theory)
+(require math/number-theory
+         math/matrix)
+
+;; This is bottle neck 
+(define (factorial-mod l h p)
+  (for/fold ([acc 1])
+            ([i (in-range l (+ h 1))])
+    (modulo (* acc i) p)))
+
 
 (define (prime-base n p)
   (cond
@@ -8,6 +16,7 @@
      (let-values ([(quot rem) (quotient/remainder n p)])
        (cons rem (prime-base quot p)))]))
 
+
 (define (zip-fun mp np)
   (match (list mp np)
     [(list '() '()) '()]
@@ -15,29 +24,57 @@
     [(list '() (cons n nrest)) (cons (cons 0 n) (zip-fun '() nrest))]
     [(list (cons m mrest) (cons n nrest)) (cons (cons m n) (zip-fun mrest nrest))]))
 
-(define (inverse-euler n p)
-  (modular-expt n (- p 2) p))
 
-(define (factorial-mod n p)
-  (if (< n 0) 0
-      (for/fold ([acc 1])
-                ([i (in-range 1 (+ n 1))])
-        (modulo (* acc i) p))))
-        
+(define (inverse-euler n p)
+  (modular-inverse n p))
+
 
 (define (binom-mod mi ni p)
-  (modulo (* (factorial-mod mi p) (inverse-euler (factorial-mod ni p) p) (inverse-euler (factorial-mod (- mi ni) p) p)) p)) 
-  
+  (if (< mi ni) 0
+      (let* ([k (max (- mi ni) ni)]
+             [l (min (- mi ni) ni)]
+             [uval (factorial-mod (+ k 1) mi p)]
+             [lval (factorial-mod 1 l p)])
+        (modulo (* uval (inverse-euler lval p)) p))))
+
 ;(define (binom-mod mi ni p)
 ;  (modulo (binomial mi ni) p))
-  
-(define (lucas-binom m n p)
+
+(define (lucas-binom-mod-prime m n p)
   (let* ([mp (prime-base m p)]
          [np (prime-base n p)]
          [mnp (zip-fun mp np)])
-   (foldl (λ(xy init) (modulo (* init (binom-mod (car xy) (cdr xy) p)) p)) 1 mnp)))
+    (foldl (λ(xy init) (modulo (* init (binom-mod (car xy) (cdr xy) p)) p))
+           1 mnp)))
 
-(lucas-binom 950 100 7) ; 2
-(lucas-binom 10 2 13); 6
-(lucas-binom 1000 900 13); 8
-(lucas-binom 456 51 5)
+;(lucas-binom-mod-prime 950 100 7) ; 2
+;(lucas-binom-mod-prime 10 2 13); 6
+;(lucas-binom-mod-prime 1000 900 13); 8
+;(lucas-binom-mod-prime (expt 10 9)  (expt 10 8) (+ 7 (expt 10 9)))
+
+;; M is square free
+(define (lucas-binom-mod-square-free-num m n M)
+  (let* ([fact (prime-divisors M)]
+         [luca (map (λ(p) (lucas-binom-mod-prime m n p)) fact)])
+    (solve-chinese luca fact)))
+
+
+(let loop ()
+  (define a (string->number (read-line (current-input-port) 'any)))
+  (let/ec break
+    (define (repeat cnt)
+      (cond
+        [(>= cnt a) (break)]
+        [else
+         (define b (read-line (current-input-port) 'any))
+         (define in (map string->number (string-split b)))
+         (define m (car in))
+         (define n (cadr in))
+         (define M (caddr in))
+         (fprintf (current-output-port) "~a\n"
+                  (lucas-binom-mod-square-free-num m n M))
+         (repeat (+ 1 cnt))]))
+    (repeat 0)))
+
+;; Tested for 
+;; https://www.hackerrank.com/contests/infinitum10/challenges/cheese-and-random-toppings/problem
